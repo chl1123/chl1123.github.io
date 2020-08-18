b22886d63259a674b9e13f03b1b99a29b18e35e9
huangqiangsheng <huangqiangsheng9@gmail.com>
2020-08-18 21:27:44 +0800

    增加固定路径导航的脚本

---
+++ module/goPath.py
@@ -0,0 +1,29 @@
+import json
+import time
+from rbk import MoveStatus, BasicModule
+
+class Module(BasicModule):
+    def __init__(self, r, args):
+        super(Module, self).__init__()
+        self.goal = [0,0,0]
+        self.init = False
+        self.status = MoveStatus.RUNNING
+    def run(self, r,args):
+        self.status = MoveStatus.RUNNING
+        if not self.init:
+            self.init = True
+            if "x" in args:
+                self.goal[0] = float(args["x"])
+            if "y" in args:
+                self.goal[1] = float(args["y"])
+            if "theta" in args:
+                self.goal[2] = float(args["theta"])
+            r.logInfo("goal: " + str(self.goal))
+            r.resetPath()
+            r.setPathOnRobot([0,self.goal[0]], [0, self.goal[1]], self.goal[2])
+        r.goPath()
+        if r.isPathReached():
+            self.status = MoveStatus.FINISHED
+        else:
+            self.status = MoveStatus.RUNNING
+        return self.status.value
