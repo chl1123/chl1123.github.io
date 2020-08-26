e1376cfedfe0e67b7ece74f9048b14c54d1bd0a6
huangqiangsheng <huangqiangsheng9@gmail.com>
2020-08-26 18:27:24 +0800

    去除不必要的log

--- module/resetShelf.py
+++ module/resetShelf.py
@@ -6,10 +6,6 @@ class Module(BasicModule):
     def __init__(self, r, args):
         super(Module, self).__init__()
     def run(self, r,args):
-        di = r.Di()
-        print(r.navSpeed())
-        for (ind,node) in enumerate(di['node']):
-            print("di:" , ind, node)
         self.status = MoveStatus.FINISHED
         r.resetLocalShelfArea()
         return self.status.value
