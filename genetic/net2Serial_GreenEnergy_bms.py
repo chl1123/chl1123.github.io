b2840fa923f998ad54abeade77165b64dd46e9a6
darboy <974782852@qq.com>
2021-08-13 10:16:44 +0800

    fix:修改韩国电池协议

--- genetic/net2Serial_GreenEnergy_bms.py
+++ genetic/net2Serial_GreenEnergy_bms.py
@@ -1,59 +1,43 @@
 import sys
 import os
+sys.path.append('/usr/local/etc/.SeerRobotics/rbk/resources/scripts/site-packages')
 #导入电池基类
-import syspy.battery.battery_base as bb
-#处理字符的工具类
-import syspy.lib.char_utility as cu
-#其他工具类,如定时器
-import syspy.lib.misc_utility as mu
-#crc16
-def crc16_cal(datalist):#crc16 check
-    test_crc=0xFFFF            
-    poly=0xa001
-    # poly=0x8005
-    numl=len(datalist)
-    for num in range(numl):
-        data=datalist[num]
-        test_crc=(data&0xFF)^test_crc 
-        for bit in range(8):
-            if(test_crc&0x1)!=0:
-                test_crc>>=1
-                test_crc^=poly
-            else:
-                test_crc>>=1
+import syspy.battery.battery_base as bb 
+#处理字符的工具类  
+import syspy.lib.char_utility as cu 
+#其他工具类,如定时器 
+import syspy.lib.misc_utility as mu 
 
-    return test_crc&0xff,(test_crc>>8)&0xff
 
 class testBattery(bb.batteryBase):
     """
-    Inherit the battery base class
+    继承电池基类
     """
     def __init__(self):
         #Initialize the base class
         super(testBattery,self).__init__()
         #create a data buffer for saveing data
         self.packeddata_buff = []
-    
         self.realdata_buff = [] # this number depends on how many realdata message we will receive
-        
-    # Mark whether the data has been received correctly
+        # Mark whether the data has been received correctly
         self.msg_ok = False
-
-        # Create a protocol object with battery information
+        self.rec_flag=[False,False,False]
+        self.soc = 0
+        self.voltage = 0
+        self.current = 0
+        self.max_temp = 0 
 
     def handleData(self, msg:list):
         """
         Handle the recive data
         """
         #save to buffer
-        for i in range(len(msg)):
+        for i in range(0, len(msg)):
             self.packeddata_buff.append(int(msg[i]))
-        print("")
         # change data type to int
-
         if self.packeddata_buff[0]==0x02:
             if self.packeddata_buff[-1]==0x03:
-                #self.msg_ok = True
+                # self.msg_ok = True
                 for index in range(1,len(self.packeddata_buff)-1):
                     if self.packeddata_buff[index]==0x10 and self.packeddata_buff[index+1]==0x82:
                         self.packeddata_buff[index+1]=0x02
@@ -71,10 +55,8 @@ class testBattery(bb.batteryBase):
                     self.realdata_buff=[]
                     self.packeddata_buff=[]
 
-
             elif self.packeddata_buff[-1]!=0x03 and len(self.packeddata_buff)>26: #
                 self.packeddata_buff=[]
-
         else:
             self.packeddata_buff=[]
 
@@ -82,54 +64,62 @@ class testBattery(bb.batteryBase):
             """
              write parse code
             """
-            self.battery_info.percetage = percetage  
-            self.battery_info.temperature = temperature
-            self.battery_info.charge_current = current
-            self.battery_info.charge_voltage = voltage
+
+            ID = cu.merge4bytesTo1(self.realdata_buff[3], self.realdata_buff[2],self.realdata_buff[1],self.realdata_buff[0])
+            if (ID == 0x351): #Summary
+                self.soc = cu.merge2bytesTo1(self.realdata_buff[5], self.realdata_buff[4])*0.001 #SOC
+                self.rec_flag[0]=True
+                #print(self.soc)
+
+            elif (ID == 0x352): #PackValue
+                self.voltage = cu.merge2bytesTo1(self.realdata_buff[5], self.realdata_buff[4])*0.1 #P_Volt
+                self.current  = cu.u16Toint16(cu.merge2bytesTo1(self.realdata_buff[7], self.realdata_buff[6]))*0.1 #P_Curr
+                self.rec_flag[1]=True
+                #print("voltage :", self.voltage, "current :", self.current)
+
+            elif (ID == 0x354): #Temperature
+                self.max_temp = cu.u8Toint8(self.realdata_buff[0]) 
+                self.rec_flag[2]=True
+                #print("max temp :", self.max_temp)
+
+            self.realdata_buff = []
+            
+            if True in self.rec_flag:
+                self.clearTimeout()
+                self.connect_timeout_t.reset()
+
+        if not False in self.rec_flag:
+            self.battery_info.percetage = self.soc #percentage
+            self.battery_info.temperature = self.max_temp #temperature
+            self.battery_info.charge_current = self.current
+            self.battery_info.charge_voltage = self.voltage
             #发步电池数据给rbk
             self.publish(self.battery_info)  
             #清空缓冲区列表
-            self.realdata_buff = []
-            #标记该次数据接收完成且正确
-            self.msg_ok = True
-
-
+            self.rec_flag=[False,False,False]
+            #when receive all right information
+            self.clearTimeout()
+            self.connect_timeout_t.reset()
 
     def loop(self):
         """
-        Loop,and handle send package and timeout logic
+        循环,处理发送及超时逻辑
         """
+        #创建一个超时定时器
         self.battery_info = self.createBatteryMessage()
-        #create a timeout timer
-        connect_timeout_t = mu.Timer(2000)
-        # send package, use dictionary type to classify
-     # send package, use dictionary type to classify
-        # #first byte is slave address
-        sendmsg=[0x02,0x10,0x00,0x00,0x01,0x1E,0x2D,0x03]
-        
-        connect_timeout_t.reset()
+        self.clearTimeout()
+
+        self.connect_timeout_t = mu.Timer(4000)
+        request = [0x02, 0x01, 0x00, 0x00, 0x01, 0x1E, 0x2D, 0x03]
+        self.send(request)
+
         while True:
-                self.send(sendmsg)
-
-                #Wait for one data package has been received or not.
-                while not self.msg_ok:
-                    # If timer is time up, it will report a timeout and enter the next cycle
-                    if connect_timeout_t.isTimeUp():
-                        self.setTimeout()
-                        break
-                #if one data package has been received completly
-                if self.msg_ok:
-                    #Clear timeout error, reset flag bit
-                    self.clearTimeout()
-                    self.msg_ok = False
-                    connect_timeout_t.reset()
-
-                mu.sleep_s(1)
-            
-
+            mu.sleep_ms(10)
+            if self.connect_timeout_t.isTimeUp():
+                self.connect_timeout_t.reset()
+                self.setTimeout()
 
 if __name__ == '__main__':
     client = testBattery()
     client.loop()
-
     
