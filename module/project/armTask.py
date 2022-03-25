f040b1c1a778a897a9f874864f3802faf18ce8a1
qiangsheng <qiangsheng.huang@foxmail.com>
2022-03-25 17:33:35 +0800

    issue_pool#3020 机械臂的脚本控制v2

--- module/project/armTask.py
+++ module/project/armTask.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# @Time : 2022/3/15
+# @Time : 2022/3/24
 # @Author : qiangsheng
 # @File : armTask.py
 # @Request : issue_pool#3020 荣成拼合单
@@ -70,12 +70,11 @@ class Module(BasicModule):
         super(Module, self).__init__()
         self.armFile = "armData.json"
         self.armData = None
-        self.type2c = dict() #类型到字典的数据
 
         with open(self.armFile) as fp:
             self.armData = json.load(fp)
-        for k,v in self.armData.items():
 
+        for k,v in self.armData.items():
             if "load" not in v:
                 r.setError("container {} load is missing.".format(k))
                 self.type2c = dict()
@@ -84,18 +83,10 @@ class Module(BasicModule):
                 r.setError("container {} unload is missing.".format(k))
                 self.type2c = dict()
                 break
-            gtype = 0
-            if "goodsType" in v:
-                gtype = v["goodsType"]
-            if gtype in self.type2c:
-                self.type2c[gtype].append(k)
-            else:
-                self.type2c[gtype] = [k]
 
         self.operation = None
         self.goodsId = None
         self.container = None
-        self.goodsType = None
 
         self.cmd = None
         self.init = True
@@ -114,86 +105,43 @@ class Module(BasicModule):
         if self.armData == None:
             r.setError("cannot load {}".format(self.armFile))
             return self.failTask(r)
-        if len(self.type2c) < 1:
-            r.setError("{} format is error".format(self.armFile))
-            return self.failTask(r)
         armInfo = r.getArmInfo()
         if self.init:
             self.init = False
             self.start_time = time.time()
             self.operation = args.get("operation",None)
             self.container = args.get("container",None)
-            self.goodsType = args.get("goodsType",0)
             self.getArmArgsAndGoodsId(r)
-            if self.operation != "load" and self.operation != "unload":
-                r.setError("operation is wrong {}.".format(self.operation))
-                return self.failTask(r)
             if self.armArgs is None:
                 r.setError("armArgs is missing.")
                 return self.failTask(r)
-            # 加载货物没有给goodsId
-            if self.goodsId == "" and self.goodsId is None and self.operation == "load":
-                r.setError("goodsId is wrong {}.".format(self.goodsId))
-                return self.failTask(r)
-
-            # 货物类型传错了
-            if self.goodsType is not None and \
-                self.goodsType not in self.type2c:
-                r.setError("goodsType is wrong {}.".format(self.goodsType))
-                return self.failTask(r)
-
-            # 获取背篓信息
-            cur_containers = r.getContainers()
-            cur_cs = dict()
-            for v in cur_containers:
-                cur_cs[v["container_name"]] = v
-
-            if self.container is not None:
-                # 背篓名称错误
-                if self.container not in self.armData:
-                    r.setError("container is wrong {} is not in the {}.".format(self.container, self.armFile))
-                    return self.failTask(r)
-                c = self.armData[self.container]
-                
-                # operation 错误  
-                if self.operation not in c:
-                    r.setError("container {} and operation {} is mismatch.".format(self.container, self.operation))
-                    return self.failTask(r)     
-                
-                # container 没有机器人身上               
-                if self.container not in cur_cs:
-                    r.setError("container {} is not in the robot {}.".format(self.container, str(list(cur_containers.keys()))))
+            self.taskid = armInfo["taskid"] + 1
+            if self.operation == "load" or self.operation == "unload":
+                if not isinstance(self.armArgs, list):
+                    r.setError("armArgs should be list. {}".format(self.armArgs))
+                scriptInsert_num = 0
+                for a in self.armArgs:
+                    if "type" in a:
+                        if a["type"] == "ScriptInsert":
+                            scriptInsert_num += 1
+                if scriptInsert_num != 1:
+                    r.setError("armArgs should have only one ScriptInsert. {}".format(self.armArgs))
                     return self.failTask(r)
 
-                cur_c = cur_cs[self.container]
+                # 加载货物没有给goodsId
+                if self.goodsId == "" and self.goodsId is None and self.operation == "load":
+                    r.setError("goodsId is wrong {}.".format(self.goodsId))
+                    return self.failTask(r)
 
-                if self.operation == "load":
-                    # load again
-                    if cur_c["has_goods"]:
-                        r.setError("container {} has goods cannot load again.".format(self.container))
-                        return self.failTask(r)
-                elif self.operation == "unload":
-                    if self.goodsId is not None \
-                    and self.goodsId != cur_c["goods_id"]:
-                        r.setError("Unload. goodsId mismatch. input goodsId {}, container {} goodsId {}".format(self.goodsId, self.container, cur_c["goods_id"]))
-                        return self.failTask(r)
-                    pass
-                
-                # 生成指令
-                self.cmd = c[self.operation]
-            else:
+                # 获取背篓信息
+                cur_containers = r.getContainers()
+                cur_cs = dict()
+                for v in cur_containers:
+                    cur_cs[v["container_name"]] = v
+
                 # find the container
                 if self.operation == "load":
-                    if self.goodsType is None:
-                        r.setError("goodsType is missing in load.")
-                        return self.failTask(r)
-                    # 依据goodsType找到空的背篓
-                    cnames = self.type2c[self.goodsType]
-                    for cn in cnames:
-                        # 机器人没有这个背篓
-                        if cn not in cur_cs:
-                            r.setError("container {} is not in the robot {}.".format(cn, str(list(cur_cs.keys()))))
-                            return self.failTask(r)
+                    for cn in cur_cs:
                         cur_c = cur_cs[cn]
                         if cur_c["has_goods"] == False:
                             self.container = cn
@@ -241,19 +189,20 @@ class Module(BasicModule):
                         return self.failTask(r)    
 
                     self.cmd= c[self.operation]
-            if self.operation == "load":
-                if not isinstance(self.container, str) or not isinstance(self.goodsId, str):
-                    r.setError("type error. container {}, goodsId {}".format(type(self.container), type(self.goodsId)))
-                    return self.failTask(r)
-            elif self.operation == "unload":
-                if not isinstance(self.container, str):
-                    r.setError("type error. container {}".format(type(self.container)))
-                    return self.failTask(r)
-            self.taskid = armInfo["taskid"] + 1
-            # armBinTask这个函数可能会有变化
-            # TODO 拼一下识别的字符串
-            self.cmd = self.armArgs + self.cmd
-            r.armBinTask(self.taskid, json.dumps(self.cmd))
+
+                # armBinTask这个函数可能会有变化
+                # TODO 拼一下识别的字符串
+                replace_num = -1
+                for n, a in enumerate(self.armArgs):
+                    if "type" in a:
+                        if a["type"] == "ScriptInsert":
+                            replace_num = n
+                if replace_num >= 0:
+                    out_args = self.armArgs
+                    out_args[replace_num] = self.cmd
+                    r.armBinTask(self.taskid, json.dumps(out_args))
+            else:
+                r.armBinTask(self.taskid, json.dumps(self.armArgs))
             r.logDebug("ArmInitS][{}|{}|{}|{}|{}".format(self.container, self.operation, self.goodsId, self.taskid, self.cmd))      
         if armInfo["taskid"] == self.taskid:
             if armInfo["task_status"] == 2:
@@ -291,7 +240,7 @@ if __name__ == '__main__':
     sim = SimModule()
     print("********load********")
     args = {
-        "operation":"load"
+        "operation":"load",
     }
     print(args)
     m = Module(sim, args)
@@ -299,6 +248,13 @@ if __name__ == '__main__':
     print("********unload********")
     args = {
         "operation":"unload"
+    }
+    print(args)
+    m = Module(sim, args)
+    m.run(sim,args)
+
+    print("********no, op********")
+    args = {
     }
     print(args)
     m = Module(sim, args)
