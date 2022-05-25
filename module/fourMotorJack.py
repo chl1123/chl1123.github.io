043a2e40e0abb12a2671ade6b13f3d76984e68ff
zhong <zhonghan@seer-group.com>
2022-05-25 21:30:35 +0800

    issues#3319 多电机顶升同步安全机制检查

--- module/fourMotorJack.py
+++ module/fourMotorJack.py
@@ -8,7 +8,7 @@ import json
 
 from rbk import MoveStatus, BasicModule, ParamServer
 from rbkSim import SimModule
-from robot import Motor, MotorType, Robot, check_DI
+from robot import Motor, MotorType, Robot, ModuleTool
 
 
 """ 
@@ -50,6 +50,8 @@ class Module(BasicModule):
         self.zero_di4 = p.loadParam("ZeroDi4", type="int", default=7, comment="电机4零位DI")
         self.shelf_file = p.loadParam("ShelfFile", type="str", default="", comment="货物模型文件，默认为空")
         self.default_height = p.loadParam("DefaultHeight", type="float", default=0.01, comment="顶升默认上升高度")
+        self.extremum_diff = p.loadParam("ExtremumDiff", type="float", default=0.001, comment="多电机同步时编码器极值差阈值")
+        self.variance = p.loadParam("Variance", type="float", default=1.0, comment="多电机同步时编码器方差阈值")
 
         self.init = True
         self.status = MoveStatus.NONE
@@ -66,7 +68,7 @@ class Module(BasicModule):
             if "loadHeight" in args:
                 self.height = args["loadHeight"]
                 if args["loadHeight"] <= 0:
-                    self.height = 0.00001
+                    self.height = -0.00001
             else:
                 self.height = self.default_height
             if "operation" not in args:
@@ -78,7 +80,8 @@ class Module(BasicModule):
             self.jack_motors.append(Motor(r, MotorType.LINEAR_MOTOR, self.motor_name4, -1))
 
         self.status = MoveStatus.RUNNING
-        if check_DI(r, self.up_limit_di1) or check_DI(r, self.up_limit_di2) or check_DI(r, self.up_limit_di3) or check_DI(r, self.up_limit_di4):
+        if ModuleTool.check_DI(r, self.up_limit_di1) or ModuleTool.check_DI(r, self.up_limit_di2) \
+                or ModuleTool.check_DI(r, self.up_limit_di3) or ModuleTool.check_DI(r, self.up_limit_di4):
             r.setError(f"触发上限位DI")
             self.status = MoveStatus.FAILED
         if args["operation"] == "JackLoad":
@@ -88,17 +91,25 @@ class Module(BasicModule):
         else:
             r.setError(f"operation args error: {args}")
             self.status = MoveStatus.FAILED
-
+        # 电机控制方案速度规划和下发
         r.publishSpeed()
+        # 多电机同步机制安全检查
+        motor_pos = []
+        for motor_name in [self.motor_name1, self.motor_name2, self.motor_name3, self.motor_name4]:
+            motor_pos.append(ModuleTool.get_motor_pos(r, motor_name))
+        self.motor_safe_check(r, motor_pos)
+        # ############## 数据上报 ##############
         self.state["status"] = self.status
         self.state["args"] = args
         self.state["has_goods"] = r.hasGoods()
-        # self.state["motors"] = {
-        #     self.motor_name1: {"upLimitDi": self.up_limit_di1, "zeroDi": self.zero_di1, "reach": r.isMotorReached(self.motor_name1), "stop": r.isMotorStop(self.motor_name1)},
-        #     self.motor_name2: {"upLimitDi": self.up_limit_di2, "zeroDi": self.zero_di2, "reach": r.isMotorReached(self.motor_name2), "stop": r.isMotorStop(self.motor_name2)},
-        #     self.motor_name3: {"upLimitDi": self.up_limit_di3, "zeroDi": self.zero_di3, "reach": r.isMotorReached(self.motor_name3), "stop": r.isMotorStop(self.motor_name3)},
-        #     self.motor_name4: {"upLimitDi": self.up_limit_di4, "zeroDi": self.zero_di4, "reach": r.isMotorReached(self.motor_name4), "stop": r.isMotorStop(self.motor_name4)},
-        # }
+        self.state["motor_pos_extremum_diff"] = self.get_extremum_diff(*motor_pos)
+        self.state["motor_pos_variance"] = self.get_variance(*motor_pos)
+        self.state["motors"] = {
+            self.motor_name1: {"upLimitDi": self.up_limit_di1, "zeroDi": self.zero_di1, "reach": r.isMotorReached(self.motor_name1), "stop": r.isMotorStop(self.motor_name1)},
+            self.motor_name2: {"upLimitDi": self.up_limit_di2, "zeroDi": self.zero_di2, "reach": r.isMotorReached(self.motor_name2), "stop": r.isMotorStop(self.motor_name2)},
+            self.motor_name3: {"upLimitDi": self.up_limit_di3, "zeroDi": self.zero_di3, "reach": r.isMotorReached(self.motor_name3), "stop": r.isMotorStop(self.motor_name3)},
+            self.motor_name4: {"upLimitDi": self.up_limit_di4, "zeroDi": self.zero_di4, "reach": r.isMotorReached(self.motor_name4), "stop": r.isMotorStop(self.motor_name4)},
+        }
         r.setInfo(json.dumps(self.state))
         r.logInfo(json.dumps(self.state))
         return self.status
@@ -130,6 +141,12 @@ class Module(BasicModule):
         unload_state['actions'] = self.robot.state
         self.state['operation'] = unload_state
 
+    def motor_safe_check(self, r, motor_data):
+        """多电机同步安全检查，通过极值差值和方差判断"""
+        if self.get_extremum_diff(*motor_data) > self.extremum_diff or self.get_variance(*motor_data) > self.variance:
+            r.setError(f"Motor running out of sync! Motor pos:{motor_data}")
+            self.status = MoveStatus.FAILED
+
     def cancel(self, r: SimModule):
         r.stopRobot(True)
         r.logInfo("task cancel")
@@ -139,6 +156,25 @@ class Module(BasicModule):
         r.stopRobot(False)
         r.logInfo("task suspend")
         self.status = MoveStatus.SUSPENDED
+
+    @staticmethod
+    def get_variance(*data):
+        """
+        获取一组数据的方差
+        :param data: 一组数据
+        :return: 方差
+        """
+        average = sum(data)/len(data)
+        return sum([(x - average) ** 2 for x in data]) / len(data)
+
+    @staticmethod
+    def get_extremum_diff(*data):
+        """
+        获取一组数据的极值差值
+        :param data: 一组数据
+        :return: 极值差
+        """
+        return max(data) - min(data)
 
 
 if __name__ == '__main__':
