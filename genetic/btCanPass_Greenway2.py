7572067154099f807ff91fd58a9eefb46a46f52b
lizer <lizc@seer-group.com>
2023-06-01 11:22:50 +0800

    src#643 feat：Greenway正品电池适配测试通过

---
+++ genetic/btCanPass_Greenway2.py
@@ -0,0 +1,90 @@
+import sys
+# 导入电池基类
+import syspy.battery_Can.canpass_base as cb
+# 其他工具类,如定时器
+import syspy.lib.misc_utility as mu
+import syspy.lib.udp_debug as ud
+import syspy.lib.char_utility as cu
+
+class testCanBattery(cb.canPassBase):
+
+    def __init__(self):
+        # 初始化基类,必须做
+        super(testCanBattery, self).__init__()
+        self.__debug_out = ud.udpDebug()
+        sys.stdout = self.__debug_out
+        # 用来表示数据是否已经正确接收
+        self.msg_ok = False
+        self.tem = []
+        self.battery_info = self.createBatteryMessage()
+
+    def handleData(self, msg):
+        canframe = self.recCanframe(msg)
+        if canframe.ID == 0x0EA0F40D:
+            tem = canframe.Data.hex()
+            percentage = round(int(tem[0:2], 16) * 0.01, 2)
+            cycle = int(tem[4:6] + tem[6:8], 16)
+            if int(tem[12:14], 16) == 1:
+                is_charging = True
+            else:
+                is_charging = False
+            self.battery_info.percetage = percentage
+            self.battery_info.is_charging = is_charging
+            self.battery_info.cycle = cycle
+            self.publish(self.battery_info)
+            self.msg_ok = True
+        elif canframe.ID == 0x0EA1F40D:
+            tem = canframe.Data.hex()
+            current = round(cu.hexStr_to_int(tem[0:4] + tem[4:8]) * 0.001, 2)
+            voltage = round(int(tem[8:12] + tem[12:16], 16) * 0.001, 2)
+            self.battery_info.charge_voltage = voltage
+            self.battery_info.charge_current = current
+            self.publish(self.battery_info)
+            self.msg_ok = True
+        elif canframe.ID == 0x0EA2F40D:
+            tem = canframe.Data.hex()
+            temperature = round(int(tem[4:6], 16) - 40, 2)
+            self.battery_info.temperature = temperature
+            self.publish(self.battery_info)
+            self.msg_ok = True
+        elif canframe.ID == 0x0EA4F40D:
+            tem = canframe.Data.hex()
+            max_charge_voltage = round(int(tem[0:2] + tem[2:4], 16) * 0.01, 2)
+            max_charge_current = round(int(tem[4:6] + tem[6:8], 16) * 0.01, 2)
+            self.battery_info.max_charge_current = max_charge_current
+            self.battery_info.max_charge_voltage = max_charge_voltage
+            self.publish(self.battery_info)
+            self.msg_ok = True
+
+    def loop(self):
+        # 创建一个超时定时器
+        connect_timeout_t = mu.Timer(2000)
+        # 需要至少7s来等待底层初始化,否则将会覆盖操作
+        mu.sleep_s(7)
+        self.attachCanID(2, True, 4, 0x0EA0F40D, 0x0EA1F40D, 0x0EA2F40D, 0x0EA4F40D)
+        self.battery_info = self.createBatteryMessage()
+        while True:
+            # 判断是否收到整包
+            if self.msg_ok:
+                # 清除超时错误,重置标志位
+                self.clearTimeout()
+                self.msg_ok = False
+                connect_timeout_t.reset()
+            # 等待是否收到整包,若超时则报超时,并进入下次循环
+            while not self.msg_ok:
+                if connect_timeout_t.isTimeUp():
+                    self.setTimeout()
+                    break
+            mu.sleep_s(2)
+
+if __name__ == '__main__':
+    client = testCanBattery()
+    client.loop()
+
+
+
+
+
+
+
+
