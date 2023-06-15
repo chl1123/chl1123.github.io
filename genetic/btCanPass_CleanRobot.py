abb470cc300ca5fa25b468c80a2788c2b26f323e
lizer <lizc@seer-group.com>
2023-06-15 15:28:10 +0800

    src#691 fix：优化通讯逻辑

--- genetic/btCanPass_CleanRobot.py
+++ genetic/btCanPass_CleanRobot.py
@@ -14,18 +14,19 @@ class testCanBattery(cb.canPassBase):
         super(testCanBattery, self).__init__()
         self.__debug_out = ud.udpDebug()
         sys.stdout = self.__debug_out
+        # 创建一个超时定时器
+        self.connect_timeout_t = mu.Timer(2000)
         # 用来表示数据是否已经正确接收
         self.msg_ok = False
         self.tem = []
         self.battery_info = self.createBatteryMessage()
 
     def handleData(self, msg):
+        self.clearTimeout()
         canframe = self.recCanframe(msg)
         tem = canframe.Data.hex()
         if canframe.ID == 0x112:
-            print(tem)
-            # 取date部分值将hex转int（根据实际协议自行设定，此处为示例）
-            voltage = round(int(tem[0:2] + tem[2:4], 16)* 0.1, 2)
+            voltage = round(int(tem[0:2] + tem[2:4], 16) * 0.1, 2)
             percentage = round(int(tem[8:10], 16) * 0.004, 2)
             temperature = round(int(tem[10:12], 16)-40, 2)
             current = round(cu.hexStr_to_int(tem[4:6] + tem[6:8],16) * 0.1, 2)
@@ -40,27 +41,23 @@ class testCanBattery(cb.canPassBase):
             self.battery_info.is_charging = is_charging
             self.publish(self.battery_info)
             self.msg_ok = True
-        # 发步电池数据给rbk
+
+    def judgeMsgok(self):
+        if self.msg_ok:
+            # 清除超时错误,重置标志位
+            self.msg_ok = False
+            self.connect_timeout_t.reset()
+        else:
+            if self.connect_timeout_t.isTimeUp():
+                self.setTimeout()
 
     def loop(self):
-        # 创建一个超时定时器
-        connect_timeout_t = mu.Timer(2000)
-        # 需要至少7s来等待底层初始化,否则将会覆盖操作
-        mu.sleep_s(7)
-        self.attachCanID(1, False, 1, 0x112, 0, 0, 0)
+        # 需要至少5s来等待底层初始化,否则将会覆盖操作
+        mu.sleep_s(5)
+        self.attachCanID(2, False, 1, 0x112, 0, 0, 0)
         self.battery_info = self.createBatteryMessage()
         while True:
-            # 判断是否收到整包
-            if self.msg_ok:
-                # 清除超时错误,重置标志位
-                self.clearTimeout()
-                self.msg_ok = False
-                connect_timeout_t.reset()
-            # 等待是否收到整包,若超时则报超时,并进入下次循环
-            while not self.msg_ok:
-                if connect_timeout_t.isTimeUp():
-                    self.setTimeout()
-                    break
+            self.judgeMsgok()
             mu.sleep_s(2)
 
 if __name__ == '__main__':
