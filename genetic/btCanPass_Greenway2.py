4c3d7531080cdb10a81542d006ddba31c3f06f2c
lizer <lizc@seer-group.com>
2023-06-15 15:36:55 +0800

    test_center#2311 feat：针对正品电池修复存在问题，优化通讯逻辑

--- genetic/btCanPass_Greenway2.py
+++ genetic/btCanPass_Greenway2.py
@@ -14,12 +14,14 @@ class testCanBattery(cb.canPassBase):
         self.__debug_out = ud.udpDebug()
         sys.stdout = self.__debug_out
         # 用来表示数据是否已经正确接收
+        self.battery_info = self.createBatteryMessage()
+        self.connect_timeout_t = mu.Timer(5000)
         self.msg_ok = False
         self.tem = []
-        self.battery_info = self.createBatteryMessage()
 
     def handleData(self, msg):
         canframe = self.recCanframe(msg)
+        self.clearTimeout()
         if canframe.ID == 0x0EA0F40D:
             tem = canframe.Data.hex()
             percentage = round(int(tem[0:2], 16) * 0.01, 2)
@@ -35,7 +37,7 @@ class testCanBattery(cb.canPassBase):
             self.msg_ok = True
         elif canframe.ID == 0x0EA1F40D:
             tem = canframe.Data.hex()
-            current = round(cu.hexStr_to_int(tem[0:4] + tem[4:8]) * 0.001, 2)
+            current = round(cu.hexStr_to_int(tem[0:4] + tem[4:8], 18) * 0.001, 2)
             voltage = round(int(tem[8:12] + tem[12:16], 16) * 0.001, 2)
             self.battery_info.charge_voltage = voltage
             self.battery_info.charge_current = current
@@ -48,33 +50,33 @@ class testCanBattery(cb.canPassBase):
             self.publish(self.battery_info)
             self.msg_ok = True
         elif canframe.ID == 0x0EA4F40D:
-            tem = canframe.Data.hex()
-            max_charge_voltage = round(int(tem[0:2] + tem[2:4], 16) * 0.01, 2)
-            max_charge_current = round(int(tem[4:6] + tem[6:8], 16) * 0.01, 2)
-            self.battery_info.max_charge_current = max_charge_current
-            self.battery_info.max_charge_voltage = max_charge_voltage
-            self.publish(self.battery_info)
+            if self.isNeedCharge():
+                print("start charge")
+                tem = canframe.Data.hex()
+                max_battery_info = self.createBatteryMessage()
+                max_charge_voltage = round(int(tem[0:2] + tem[2:4], 16) * 0.01, 2)
+                max_charge_current = round(int(tem[4:6] + tem[6:8], 16) * 0.01, 2)
+                max_battery_info.max_charge_current = max_charge_current
+                max_battery_info.max_charge_voltage = max_charge_voltage
+                self.publish(max_battery_info)
             self.msg_ok = True
 
+    def judgeMsgok(self):
+        if self.msg_ok:
+            # 清除超时错误,重置标志位
+            self.msg_ok = False
+            self.connect_timeout_t.reset()
+        else:
+            if self.connect_timeout_t.isTimeUp():
+                self.setTimeout()
+
     def loop(self):
-        # 创建一个超时定时器
-        connect_timeout_t = mu.Timer(2000)
         # 需要至少7s来等待底层初始化,否则将会覆盖操作
         mu.sleep_s(7)
         self.attachCanID(2, True, 4, 0x0EA0F40D, 0x0EA1F40D, 0x0EA2F40D, 0x0EA4F40D)
         self.battery_info = self.createBatteryMessage()
         while True:
-            # 判断是否收到整包
-            if self.msg_ok:
-                # 清除超时错误,重置标志位
-                self.clearTimeout()
-                self.msg_ok = False
-                connect_timeout_t.reset()
-            # 等待是否收到整包,若超时则报超时,并进入下次循环
-            while not self.msg_ok:
-                if connect_timeout_t.isTimeUp():
-                    self.setTimeout()
-                    break
+            self.judgeMsgok()
             mu.sleep_s(2)
 
 if __name__ == '__main__':
