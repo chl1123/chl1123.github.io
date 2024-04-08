da93acc343a43881e78419bd03f535cd3c85ce89
zhonghan@seer-group.com <zhonghan@seer-group.com>
2024-04-08 15:46:33 +0800

    test_center#3139 增加机制：加水时任务被中断, 关闭水阀

--- module/cleanRobot/clean_robot.py
+++ module/cleanRobot/clean_robot.py
@@ -169,6 +169,8 @@ class Module(BasicModule):
         if task_status in [3, 5, 6]:
             if not self.stop_ok:
                 self.stopV1(r)
+                r.setDO(self.addingWater_do, False)
+                # self.send_msg(r, "2B 80 30 07 00 00 00 00")
                 if self.stop_number >= 8:
                     self.stop_ok = True
                     self.stop_number = 0
@@ -207,9 +209,11 @@ class Module(BasicModule):
     #     # r.logInfo("script suspend")
     #     self.status = MoveStatus.SUSPENDED
     #
-    # def cancel(self, r: SimModule):
-    #     # r.logInfo("script cancel")
-    #     self.status = MoveStatus.NONE
+    def cancel(self, r: SimModule):
+        r.logInfo("script cancel")
+        r.setDO(self.addingWater_do, False)
+        self.send_msg(r, "2B 80 30 07 00 00 00 00")
+        self.status = MoveStatus.NONE
 
     def run(self, r: SimModule, args):
         self.status = MoveStatus.RUNNING
