c54a4f8cf21fb511bfe7206a707fc32559a0d9e9
suguoqing <leven@seer-group.com>
2024-04-29 13:29:28 +0800

    + well-wit#255, 开关机脚本

---
+++ genetic/startup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Date : 2024/04/25
+# @Author : su
+# @Version : 1.0
+# @Update :
+
+import json
+import time
+
+import subprocess
+
+# Cannot be blocked
+
+if __name__ == '__main__': 
+    # command = f"aplay /usr/local/etc/.SeerRobotics/rbk/resources/sounds/startup.wav"
+    # result = subprocess.run(command, shell=True)   
+    # print("startup.py", command, result.returncode)
+
+    pass
