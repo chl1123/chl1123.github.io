ca6851b194a8f3fcdbe13aed306bec1276a03981
Lizer <lizc@seer-group.com>
2024-06-17 10:46:15 +0800

    feat: 补充serial arm demo

--- genetic/net2Serial_demo_arm.py
+++ genetic/net2Serial_demo_arm.py
@@ -1,10 +1,13 @@
+import sys
+import os
 
 #导入电池基类
 import syspy.battery_Serial.battery_base as bb
-#处理字符的工具类  
-import syspy.lib.char_utility as cu 
-#其他工具类,如定时器 
-import syspy.lib.misc_utility as mu 
+#处理字符的工具类，处理字符的工具类，如将uint16_t的数据转换成int16_t,
+#用途:负号转换，将两个字节数据组合成一个16位的数据，其他数据处理需要自行编写。
+import syspy.lib.char_utility as cu
+#其他工具类,如定时器
+import syspy.lib.misc_utility as mu
 
 class testBattery(bb.batteryBase):
     """
@@ -13,12 +16,12 @@ class testBattery(bb.batteryBase):
     def __init__(self):
         #初始化基类,必须做
         super(testBattery,self).__init__()
-        # self.createSerial('/dev/ttyS8', 9600) #880
-        self.createSerial('/dev/ttyUart0', 9600) #800,3000
+        # aarch64穿透需要初始化串口信息，880控制器串口uart0对应/dev/ttyS8
+        self.createSerial('/dev/ttyS8', 9600)
         #创建一个超时定时器
         self.connect_timeout_t = mu.Timer(2000)
         #创建一个列表用来缓冲接收数据
-        self.data_buff = [] 
+        self.data_buff = []
         #用来表示数据是否已经正确接收
         self.msg_ok = False
 
@@ -28,12 +31,12 @@ class testBattery(bb.batteryBase):
         如下为示例
         """
         #存入收到的数据到缓冲中
-        for i in range(0, len(msg)):
-            self.data_buff.append(msg[i])
+        self.data_buff.extend(msg)
+        print(len(self.data_buff))
         #判断报文长度
-        if len(self.data_buff) >= 105:  
+        while len(self.data_buff) >= 105:
             #校验帧头是否正确
-            if self.data_buff[0] == 0x01:  
+            if self.data_buff[0] == 0x01:
                 #转换电池数据
                 voltage = cu.merge2bytesTo1(self.data_buff[39],self.data_buff[40]) * 0.01
                 current = cu.u16Toint16(cu.merge2bytesTo1(self.data_buff[41],self.data_buff[42])) * 0.01    #是int16类型
@@ -44,21 +47,28 @@ class testBattery(bb.batteryBase):
                     temperature = temp16_2
                 percetage = cu.merge2bytesTo1(self.data_buff[83],self.data_buff[84]) * 0.01
                 #创建一个电池信息的proto对象
-                battery_info = self.createBatteryMessage() 
+                battery_info = self.createBatteryMessage()
                 #解析后塞入相应字段
-                battery_info.percetage = percetage  
+                battery_info.percetage = percetage
                 battery_info.temperature = temperature
                 battery_info.charge_current = current
                 battery_info.charge_voltage = voltage
                 #发步电池数据给rbk
-                self.publish(battery_info)  
+                self.publish(battery_info)
+                # 清除超时报警
+                self.clearTimeout()
                 #清空缓冲区列表
                 self.data_buff = []
                 #标记该次数据接收完成且正确
                 self.msg_ok = True
-                self.clearTimeout()
+                print("finish")
+            else:
+                # 第一个字节有误则去除
+                self.data_buff.pop(0)
+
 
     def judgeMsgok(self):
+        # 超时检测函数
         if self.msg_ok:
             # 清除超时错误,重置标志位
             self.msg_ok = False
@@ -72,14 +82,14 @@ class testBattery(bb.batteryBase):
         循环,处理发送及超时逻辑
         """
         while True:
-            #初始化查询报文list
+            # 初始化查询报文list
             request = [0x01, 0x03, 0x00, 0x00, 0x00, 0x32, 0xC4, 0x1F]
-            #发送查询报文
+            # 发送查询报文
             self.send(request)
+            # 循环检测是否超时
             self.judgeMsgok()
             mu.sleep_s(2)
 
 if __name__ == '__main__':
     client = testBattery()
     client.loop()
-    
