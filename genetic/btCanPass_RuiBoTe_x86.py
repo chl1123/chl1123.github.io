d857876338e62ccfde8aeefd7d02d4516e2eaf03
Lizer <lizc@seer-group.com>
2024-07-23 17:58:01 +0800

    order_issue_pool#7712 feat:新增x86与arm的瑞波特电池脚本及proto修改

---
+++ genetic/btCanPass_RuiBoTe_x86.py
@@ -0,0 +1,107 @@
+import sys
+# 导入电池基类
+import syspy.battery_Can.canpass_base as cb
+# 其他工具类,如定时器
+import syspy.lib.misc_utility as mu
+import syspy.lib.udp_debug as ud
+
+
+
+class RuiBoTe(cb.canPassBase):
+
+    def __init__(self):
+        super(RuiBoTe, self).__init__()
+        self.__debug_out = ud.udpDebug()
+        sys.stdout = self.__debug_out
+        self.battery_info = self.createBatteryMessage()
+        self.connect_timeout_t = mu.Timer(7000)
+        self.msg_ok = False
+        self.open_charge = False
+
+
+    def handleData(self, msg):
+        self.judgeCanframe(msg)
+        self.publish(self.battery_info)
+
+    def judgeCanframe(self, msg):
+        canframe = self.recCanframe(msg)
+        if canframe.ID == 0x18FFA2F4:
+            self.clearTimeout()
+            tem = canframe.Data.hex()
+            current = -round(int(tem[6:8] + tem[4:6], 16) * 0.1 - 400 , 2)
+            voltage = round(int(tem[2:4] + tem[0:2], 16) * 0.1, 2)
+            percentage = round(int(tem[8:10], 16) * 0.01, 2)
+            self.battery_info.charge_current = current
+            self.battery_info.charge_voltage = voltage
+            self.battery_info.percetage = percentage
+            self.msg_ok = True
+        elif canframe.ID == 0x18FFA5F4:
+            self.clearTimeout()
+            tem = canframe.Data.hex()
+            temperature = round(int(tem[0:2], 16) - 50, 2)
+            self.battery_info.temperature = temperature
+            self.msg_ok = True
+        elif canframe.ID == 0x112:
+            self.clearTimeout()
+            tem = canframe.Data.hex()
+            if int(tem[8:10], 16) == 1:
+                self.battery_info.is_charging = True
+            else:
+                self.battery_info.is_charging = False
+            self.msg_ok = True
+        elif canframe.ID == 0x111:
+            self.clearTimeout()
+            tem = canframe.Data.hex()
+            if self.isNeedCharge():
+                max_charge_voltage = round(int(tem[2:4] + tem[0:2], 16) * 0.01, 2)
+                max_charge_current = round(int(tem[6:8] + tem[4:6], 16) * 0.01, 2)
+                self.battery_info.max_charge_current = max_charge_current
+                self.battery_info.max_charge_voltage = max_charge_voltage
+                self.open_charge = False
+            else:
+                self.battery_info.max_charge_current = 0
+                self.battery_info.max_charge_voltage = 0
+                self.open_charge = True
+            self.msg_ok = True
+        elif canframe.ID == 0x1A1:
+            self.clearTimeout()
+            tem = canframe.Data.hex()
+            if int(tem[10:12], 16) == 1:
+                self.battery_info.is_manually_connected = True
+            else:
+                self.battery_info.is_manually_connected = False
+            self.msg_ok = True
+
+
+    def judgeMsgok(self):
+        if self.msg_ok:
+            self.msg_ok = False
+            self.connect_timeout_t.reset()
+        else:
+            if self.connect_timeout_t.isTimeUp():
+                self.setTimeout()
+
+    def loop(self):
+        mu.sleep_s(5)
+        #第一个传参 1 2 对应不用实际接线can通道，可按需修改
+        self.attachCanID(2, 5, 0x18FFA2F4, 0x18FFA5F4, 0x112, 0x111, 0x1A1)
+        while True:
+            if self.open_charge:
+                self.sendCanframe(2, 0x0DA20DF4, 8, True, '01 01 00 01 01 ff 00 00')
+            else:
+                self.sendCanframe(2, 0x0DA20DF4, 8, True, '01 00 01 01 00 ff 00 00')
+            self.judgeMsgok()
+            mu.sleep_s(2)
+
+
+if __name__ == '__main__':
+    client = RuiBoTe()
+    client.loop()
+
+
+
+
+
+
+
+
--- genetic/syspy/protobuf/message_battery_aarch64_pb2.py
+++ genetic/syspy/protobuf/message_battery_aarch64_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1dmessage_battery_aarch64.proto\x12\x0crbk.protocol\"\xe7\x01\n\x0fMessage_Battery\x12\x11\n\tpercetage\x18\x01 \x01(\x01\x12\x16\n\x0e\x63harge_current\x18\x02 \x01(\x01\x12\x16\n\x0e\x63harge_voltage\x18\x03 \x01(\x01\x12\x13\n\x0bis_charging\x18\x04 \x01(\x08\x12\x13\n\x0btemperature\x18\x05 \x01(\x01\x12\r\n\x05\x63ycle\x18\x06 \x01(\x05\x12\x1a\n\x12max_charge_current\x18\x07 \x01(\x01\x12\x1a\n\x12max_charge_voltage\x18\x08 \x01(\x01\x12\r\n\x05\x65xtra\x18\t \x01(\t\x12\x11\n\tuser_data\x18\x1e \x01(\x0c\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1dmessage_battery_aarch64.proto\x12\x0crbk.protocol\"\x86\x02\n\x0fMessage_Battery\x12\x11\n\tpercetage\x18\x01 \x01(\x01\x12\x16\n\x0e\x63harge_current\x18\x02 \x01(\x01\x12\x16\n\x0e\x63harge_voltage\x18\x03 \x01(\x01\x12\x13\n\x0bis_charging\x18\x04 \x01(\x08\x12\x13\n\x0btemperature\x18\x05 \x01(\x01\x12\r\n\x05\x63ycle\x18\x06 \x01(\x05\x12\x1a\n\x12max_charge_current\x18\x07 \x01(\x01\x12\x1a\n\x12max_charge_voltage\x18\x08 \x01(\x01\x12\r\n\x05\x65xtra\x18\t \x01(\t\x12\x1d\n\x15is_manually_connected\x18\n \x01(\x08\x12\x11\n\tuser_data\x18\x1e \x01(\x0c\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -22,5 +22,5 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   _globals['_MESSAGE_BATTERY']._serialized_start=48
-  _globals['_MESSAGE_BATTERY']._serialized_end=279
+  _globals['_MESSAGE_BATTERY']._serialized_end=310
 # @@protoc_insertion_point(module_scope)
