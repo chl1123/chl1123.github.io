07bb9484d14cb18a4d3c160cb6d7d62d86568b6d
chenHongli <chenhl@seer-group.com>
2025-01-08 14:06:33 +0800

    + robokit#3162, 增加Perception插件接口

--- syspy/camera.py
+++ syspy/camera.py
@@ -1,5 +1,5 @@
 from .protobuf.messsage import Message_AllCameraCloud
-from .py_ipc import Message
+from .lib.py_rpc import Message, call_service
 
 
 class Camera(Message[Message_AllCameraCloud]):
@@ -13,3 +13,19 @@ class Camera(Message[Message_AllCameraCloud]):
     _TOPIC = "rbk.protocol.Message_AllCameraCloud"
     _PLUGIN = "MultiDcamera"
     _MODEL_CLASS = Message_AllCameraCloud
+
+    @classmethod
+    @call_service(plugin_name="Perception")
+    def addDisableDepthId(cls, ids: list):
+        """禁用指定id的深度相机
+        Args:
+            ids (list): 指定的相机id列表
+        """
+        pass
+
+    @classmethod
+    @call_service(plugin_name="Perception")
+    def clearDisableDepthId(cls):
+        """清除禁用的深度相机
+        """
+        pass
--- syspy/laser.py
+++ syspy/laser.py
@@ -1,5 +1,5 @@
 from .protobuf.messsage import Message_AllLasers
-from .py_ipc import Message
+from .lib.py_rpc import Message, call_service
 
 
 class Laser(Message[Message_AllLasers]):
@@ -13,3 +13,64 @@ class Laser(Message[Message_AllLasers]):
     _TOPIC = "rbk.protocol.Message_AllLasers"
     _PLUGIN = "MultiLaser"
     _MODEL_CLASS = Message_AllLasers
+
+    @classmethod
+    @call_service(plugin_name="Perception")
+    def addDisableLaserId(cls, ids: list):
+        """禁用指定id数组的激光雷达
+        Args:
+            ids (list): 指定的激光雷达id列表
+        """
+        pass
+
+    @classmethod
+    @call_service(plugin_name="Perception")
+    def clearDisableLaserId(cls):
+        """清除禁用的激光
+        """
+        pass
+
+    @classmethod
+    @call_service(plugin_name="Perception")
+    def setLaserAngle(cls, id: int, min_angle: float, max_angle: float):
+        """设置激光角度
+        Args:
+            id (int):
+            min_angle (float):
+            max_angle (float):
+        """
+        pass
+
+    @classmethod
+    @call_service(plugin_name="Perception")
+    def clearLaserAngle(cls):
+        """清除激光角度
+        """
+        pass
+
+    @classmethod
+    @call_service(plugin_name="Perception")
+    def setLaserWidth(cls, id: int, width: float):
+        """设置激光宽度
+        Args:
+            id (int):
+            width (float):
+        """
+        pass
+
+    @classmethod
+    @call_service(plugin_name="Perception")
+    def clearLaserWidth(cls):
+        """清除激光宽度
+        """
+        pass
+
+    @classmethod
+    @call_service(plugin_name="Perception")
+    def sensorPointCloud(cls) -> dict:
+        """获得后视激光点云信息以字典类型返回
+
+        Returns:
+            dict: 具体的任务信息
+        """
+        pass
