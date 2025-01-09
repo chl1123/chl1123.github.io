881457957fe9366e8acd06f628add122ed4a636c
chenHongli <chenhl@seer-group.com>
2025-01-09 17:33:48 +0800

    + robokit#3092, 适配RBK调用python接口

--- syspy/lib/rpc_sub.py
+++ syspy/lib/rpc_sub.py
@@ -1,9 +1,11 @@
 import zmq, json, threading
-import inspect
 
 
 class zmqSub(object):
     def __init__(self):
+        self.funs = {}
+        self.script_name = ""
+
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.SUB)
         self.socket.connect("ipc:///tmp/cpp2pythonSDK_rpc.ipc")
@@ -34,87 +36,73 @@ class zmqSub(object):
             try:
                 message = self.socket.recv()  # 阻塞等待消息
                 self.data = json.loads(message.decode('utf-8'))
-
                 print(f"Sub: {self.data}")
-                print("self.script_name, ", self.script_name)
+                res = {}
                 method_name = self.data['method']
                 if method_name in ["suspend", "reset", "cancel"]:
                     for func_name, func in self.funs.items():
                         # 如果func_name以method_name结尾，则调用
                         if func_name.endswith(method_name):
                             res = func()
-                elif method_name == "update_cmd" and "name" in self.data and self.data["name"] == self.script_name:
-                    # if "name" in self.data and self.data['name'] != "":
-                    method_name = self.data['name'] + '.' + method_name
-                    if method_name in self.funs:
-                        func = self.funs[method_name]
+                elif ("name" in self.data and self.data["name"] == self.script_name):
+                    if self.data["name"] == "":
+                        register_name = method_name
+                    else:
+                        register_name = self.data['name'] + '.' + method_name
+                    if register_name in self.funs:
+                        func = self.funs[register_name]
                         args = self.data['args']
                         if args is None:
                             res = func()
                         else:
                             res = func(args)
-                        # if isinstance(args, dict):
-                        #     res = func(**args)
-                        # elif isinstance(args, list):
-                        #     res = func(*args)
-                        # else:
-                        #     res = func(args)
                     else:
-                        res = -1
+                        continue
                 else:
-                    res = -1
+                    continue
                 data = {"res": res, "code": 0}
                 print(f'data: {data}')
-                # self.socket.send(json.dumps(data).encode('utf-8'))
             except zmq.ZMQError as e:
-                # self.socket.send(json.dumps({"code": -1}).encode('utf-8'))
                 if self.__should_close.is_set():
                     break  # 关闭线程时会触发 ZMQError，结束循环
                 print('Sub loop error, zmq.ZMQError: ', e)
             except Exception as e:
-                # self.socket.send(json.dumps({"code": -1}).encode('utf-8'))
                 print('Sub loop error, Exception: ', e)
                 break
 
 
-class rpcStub(object):
+class rpcSub(zmqSub):
     def __init__(self):
-        self.funs = {}
-        self.script_name = None
-
-    def registerFunction(self, function, name=None, script_name=None):
-        self.script_name = script_name
-        print("self.script_name", self.script_name)
-        # 获取当前文件名
-        if name is None:
-            name = function.__name__
-        if script_name is None:
-            self.script_name = inspect.stack()[1].filename
-        script_func_name = self.script_name + '.' + name
-        print("registerFunction", script_func_name)
-        self.funs[script_func_name] = function
-        print("registerFunction", self.funs)
-
-
-class rpcSub(zmqSub, rpcStub):
-    def __init__(self):
-        rpcStub.__init__(self)
         zmqSub.__init__(self)
+
+    def registerFunction(self, function, method_name="", script_name=""):
+        """注册python方法，以便RBK调用
+        Args:
+            function (): 方法
+            method_name (str): 方法名
+            script_name (str): 脚本名
+        """
+        self.script_name = script_name
+        # 参数name 为空，则取函数名
+        if method_name == "":
+            method_name = function.__name__
+        # 如果 script_name 为空，则取函数名
+        if script_name == "":
+            register_name = method_name
+        else:  # 否则，取脚本名.函数名
+            register_name = script_name + '.' + method_name
+        print("registerFunction", register_name)
+        self.funs[register_name] = function
+        print("registerFunction", self.funs)
 
 
 if __name__ == '__main__':
     class Test:
-        def setOn(self, args, param):
-            print("is me ", args)
-            print("is me ", param)
-            return 123
-
-        def exit(self):
-            print("exit")
-            # sys.exit()
+        def setChargeStateOn(self, args):
+            self.need_charge = True
+            print("setChargeStateOn: ", args)
 
 
     a = Test()
     test = rpcSub()
-    test.registerFunction(a.setOn, "setOn")
-    test.registerFunction(a.exit, "exit")
+    test.registerFunction(a.setChargeStateOn)
