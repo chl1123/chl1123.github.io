f7c76c62570bf4947df99c5c16f61d6f845762e5
wangweike <wangwk@seer-robotics.ai>
2025-07-22 00:42:08 -0700

    m-6191902344  spinmotor脚本

---
+++ tasks/calibActionScript/spinMotorZeroCalibAction.py
@@ -0,0 +1,68 @@
+import time
+import json
+import math
+from syspy import Navigation, Logger,Module,ScriptStatus,Motor
+
+log = Logger("spinMotorZeroCalibAction")
+
+class CalibMove:
+    def __init__(self):
+        self.init = True
+
+    def run(self):
+        if self.init:
+            self.init = False
+            self.cancel = False
+            self.status = ScriptStatus.RUNNING
+            self.motor_name = Module.get_task_args("name","Motor-005")
+            self.pos = 1.0
+            # Motor.resetMotor(self.motor_name)
+            # Motor.setMotorPosition(self.motor_name, 0.0, 10.0, 0)
+            Navigation.setRobotSpinAngle(0, 2)
+
+
+
+
+        self.pos = Motor.get_motor_pos(self.motor_name)
+
+        # if Motor.isMotorReached(self.motor_name):
+        if Navigation.spinRun():
+
+            self.status = ScriptStatus.FINISHED 
+
+        # if Motor.setMotorPosition(self.motor_name, 0.0, 10.0, 0):
+        #     if math.fabs(self.pos) < 0.01:
+        #         self.status = ScriptStatus.FINISHED 
+        #     else:
+        #         self.status = ScriptStatus.RUNNING
+        # else:
+        #     self.status = ScriptStatus.RUNNING
+
+    def print(self):
+        # 实时打印
+        info = dict()
+        info["motor_name"] = self.motor_name
+        info["status"] = self.status
+        info["pos"] = self.pos
+        log.info(json.dumps(info))
+
+    def cancel(self):
+        print("cancel!!!")
+        self.cancel = True
+
+def main():
+    calib_move = CalibMove()
+    Module.init()
+    Module.set_cancel_callback(calib_move.cancel)
+    while True:
+        calib_move.run()
+        calib_move.print()
+        time.sleep(0.1)
+        if calib_move.status == ScriptStatus.FINISHED:
+            Module.set_status(ScriptStatus.FINISHED)
+            return
+        if calib_move.cancel:
+            return
+
+if __name__ == '__main__':
+    main()
