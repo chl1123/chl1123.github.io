c3e07e9bbe03989e2ba62f833dbf6548747c847e
chenHongli <chenhl@seer-group.com>
2025-07-24 15:03:48 +0800

    ~ m-6215807360 fix: 修复Do初始化模型类失败

--- syspy/dio.py
+++ syspy/dio.py
@@ -87,6 +87,7 @@ class Do(Message["Message_DO"]):
     _PLUGIN = "DSPChassis"
     _MODEL_CLASS = None
 
+    @classmethod
     def init_model_class(cls):
         if cls._MODEL_CLASS is None:
             from .protobuf import Message_DO
