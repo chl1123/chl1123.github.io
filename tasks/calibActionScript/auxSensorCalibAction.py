1a11786941b259496a7866e095c37a4b6d4a3234
wangweike <wangwk@seer-robotics.ai>
2025-07-27 22:03:52 -0700

    m-6225824299 mid360RPZ标定时传入屏蔽相机的参数

--- tasks/calibActionScript/auxSensorCalibAction.py
+++ tasks/calibActionScript/auxSensorCalibAction.py
@@ -3,7 +3,7 @@ import math
 from enum import Enum, IntEnum
 import json
 import time
-from syspy import Navigation, Logger,Module,ScriptStatus
+from syspy import Navigation, Logger,Module,ScriptStatus,Camera
 
 log = Logger("auxSensorCalibAction")
 
@@ -60,6 +60,11 @@ class CalibMove:
             self.move_dist = Module.get_task_args("L", 0.2)
             self.speed_x = Module.get_task_args("V", 0.1)
             self.cancel = False
+            self.calibType = Module.get_task_args("calibType", "")
+            self.deviceName = Module.get_task_args("deviceNameList", "")
+            if self.deviceName != "":
+                if self.calibType == "CameraMid360RPZExtrinsicCalib" or self.calibType == "CameraLocMid360RPZExtrinsicCalib":
+                    Camera.addDisableDepthStrName(self.deviceName)
 
         # 实时运行
         if self.move_action == MoveAction.Back1:
@@ -81,6 +86,8 @@ class CalibMove:
             if self.move_action != MoveAction.ActionEnd:
                 Navigation.resetOdoMove()
                 self.status = ScriptStatus.RUNNING
+            if self.status == ScriptStatus.FINISHED and self.deviceName != "":
+                Camera.clearDisableDepthStrName()
 
         return self.status
 
@@ -89,12 +96,17 @@ class CalibMove:
         info = dict()
         info["move_action"] = self.move_action
         info["status"] = self.status
+        info["move_dist"] = self.move_dist
         info["speed_x"] = self.speed_x
+        info["calibType"] = self.calibType
+        info["deviceName"] = self.deviceName
         log.info(json.dumps(info))
 
     def cancel(self):
         print("cancel!!!")
         self.cancel = True
+        if self.deviceName != "":
+            Camera.clearDisableDepthStrName()
 
 def main():
     calib_move = CalibMove()
