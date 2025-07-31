3a29ceabff1629a78b679d2d5d1460677032111b
chenHongli <chenhl@seer-group.com>
2025-07-31 20:14:45 +0800

    + m-6169030252 feat: 脚本参数读取接口增加指定文件名入参

--- syspy/lib/robot_param.py
+++ syspy/lib/robot_param.py
@@ -7,11 +7,12 @@ from syspy.lib.py_rpc import Service, default_plugin, call_service
 class RobotParam(Service):
     @classmethod
     @call_service(plugin_name="NetProtocol", func_name="getParam")
-    def getConfig(cls, app_name: str, param_path: str) -> Any:
+    def getConfig(cls, app_name: str, param_path: str, file_name="") -> Any:
         """获取机器人配置参数
         Args:
             app_name (str): App名
             param_path (str): 参数路径
+            file_name (str): 文件名。缺省则从默认文件中读取。当前只有识别有多个文件，可传入"xxx.srec"。
 
         Returns:
             Any: 参数值
@@ -57,15 +58,41 @@ class RobotParam(Service):
 
 
 if __name__ == '__main__':
+    # getConfig
     recognitionObject = RobotParam.getConfig("Recognition", "recognitionObject")
-    print(f"{recognitionObject=}")
-    shelf = RobotParam.getConfig("Recognition", "recognitionObject.shelf")
-    print(f"{shelf=}")
-    legWidth = RobotParam.getConfig("Recognition", "recognitionObject.shelf.recognitionParameter.legWidth")
-    print(f"{legWidth=}")
+    print(f"default.srec {recognitionObject=}")
+    recognitionObject = RobotParam.getConfig("Recognition", "recognitionObject", "default(1).srec")
+    print(f"default(1).srec {recognitionObject=}")
+
+    goodsHeight = RobotParam.getConfig("Recognition", "recognitionObject.shelf.goodsParameter.goodsHeight")
+    print(f"default.srec {goodsHeight=}")
+    goodsHeight = RobotParam.getConfig("Recognition", "recognitionObject.shelf.goodsParameter.goodsHeight", "default(1).srec")
+    print(f"default(1).srec {goodsHeight=}")
+
     stopConfidenceThd = RobotParam.getConfig("Localization", "localizationType.2D.stopConfidenceThd")
     print(f"{stopConfidenceThd=}")
 
+    # clone
+    charger = RobotParam.getConfig("Recognition", "recognitionObject.charger")
+    print(f"{charger=}")
+
+    recognitionSide0 = RobotParam.getConfig("Recognition", "recognitionObject.charger.recognitionSide._0")
+    print(f"{recognitionSide0=}")
+    deviceName0 = RobotParam.getConfig("Recognition",
+                                       "recognitionObject.charger.recognitionSide._0." + recognitionSide0 + ".deviceName")
+    print(f"{deviceName0=}")
+
+    recognitionSide1 = RobotParam.getConfig("Recognition", "recognitionObject.charger.recognitionSide._1")
+    print(f"{recognitionSide1=}")
+    deviceName1 = RobotParam.getConfig("Recognition", "recognitionObject.charger.recognitionSide._1.D.deviceName")
+    print(f"{deviceName1=}")
+
+    recognitionSide2 = RobotParam.getConfig("Recognition", "recognitionObject.charger.recognitionSide._2")
+    print(f"{recognitionSide2=}")
+    deviceName2 = RobotParam.getConfig("Recognition", "recognitionObject.charger.recognitionSide._2.C.deviceName")
+    print(f"{deviceName2=}")
+
+    # getDevice
     moduleType = RobotParam.getDevice("Model-000", "moduleType")
     print(f"{moduleType=}")
     liftMotor = RobotParam.getDevice("Model-000", "moduleType.liftFork.liftMotor")
