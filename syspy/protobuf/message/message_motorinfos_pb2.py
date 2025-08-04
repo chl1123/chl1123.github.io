39f085d33101794c9ea11fe63f4fbba9ffa76576
chenHongli <chenhl@seer-group.com>
2025-08-04 14:21:14 +0800

    ~ m-6189581162 fix: 修复message_motorinfos_pb2导包路径

--- syspy/protobuf/message/message_motorinfos_pb2.py
+++ syspy/protobuf/message/message_motorinfos_pb2.py
@@ -12,7 +12,7 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-import message_header_pb2 as message__header__pb2
+from . import message_header_pb2 as message__header__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
