9f4b69363fd709828a364094e854a649fc006a1c
chenHongli <chenhl@seer-group.com>
2025-08-06 20:21:25 +0800

    + m-6238773715 feat: 脚本增加trace图表、日志接口

--- syspy/lib/trace.py
+++ syspy/lib/trace.py
@@ -1,25 +1,40 @@
-from .py_rpc import Service, default_plugin, call_service
+from .py_rpc import Service, default_plugin
 
 
 @default_plugin("Trace")
 class Trace(Service):
     @classmethod
-    @call_service(func_name="scriptEventInstant")
-    def event(cls, msg: str):
-        """记录事件
+    def event(cls, msg: dict, is_print: bool = True):
+        """记录事件（弃用）
 
         Args:
-            msg (str):
+            msg (str): 日志内容。
+            is_print (bool): 是否开启print打印。默认开启。
         """
-        pass
+        if is_print:
+            print("event:", msg)
+        cls.client().call_service("Trace", "traceLog", msg)
 
     @classmethod
-    @call_service(func_name="scriptLog")
-    def data(cls, topic: str, msg: str):
-        """记录数据
+    def chart(cls, msg: dict, is_print: bool = False):
+        """记录图表数据
 
         Args:
-            topic (str):
-            msg (str):
+            msg (dict): 数据内容。根据字典的key value绘制图表。
+            is_print (bool): 是否开启print打印。默认不开启。
         """
-        pass
+        if is_print:
+            print("chart:", msg)
+        cls.client().call_service("Trace", "traceChart", msg)
+
+    @classmethod
+    def log(cls, msg: str, is_print: bool = True):
+        """记录日志
+
+        Args:
+            msg (str): 日志内容。
+            is_print (bool): 是否开启print打印。默认开启。
+        """
+        if is_print:
+            print("log:", msg)
+        cls.client().call_service("Trace", "traceLog", msg)
