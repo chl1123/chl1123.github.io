d3f809fc04b7ff1d34d7e1add0e56c33b6ed4a96
chenHongli <chenhl@seer-robotics.ai>
2025-12-02 13:38:31 +0800

    + m-6487122128 feat: 适配新版RBK版本获取方式

--- syspy/utils/param_server.py
+++ syspy/utils/param_server.py
@@ -724,7 +724,7 @@ class ParamValidator:
                 # 特殊处理COMBO_BOX_BOOL类型
                 if param.get('type') == ParamType.COMBO_BOX_BOOL:
                     for child in param.get('children', []):
-                        # 使用整数0/1表示OFF/ON
+                        # 使用布尔值 False/True 表示 OFF/ON
                         bool_value = False if child['key'] == "OFF" else True
                         self.combo_box_bool_mapping[child['key']].append(
                             {
