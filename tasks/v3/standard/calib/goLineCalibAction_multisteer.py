56b0412b937ffa4c5eb3a9981f0f3997b0075fc4
wangweike <wangwk@seer-robotics.ai>
2026-04-14 11:26:54 +0800

    m-6948265826  标定脚本增加暂停和继续任务

--- tasks/v3/standard/calib/goLineCalibAction_multisteer.py
+++ tasks/v3/standard/calib/goLineCalibAction_multisteer.py
@@ -126,10 +126,21 @@ class CalibMove:
         print("cancel!!!")
         self.cancel = True
 
+    def Suspend(self):
+        Module.setStatus(ScriptStatus.SUSPENDED)
+        print("suspend!!!")
+
+    def Resume(self):
+        Module.setStatus(ScriptStatus.RUNNING)
+        Navigation.resetOdoMove()
+        print("resume!!!")
+
 def main():
     calib_move = CalibMove()
     Module.init()
     Module.setCancelCallback(calib_move.Cancel)
+    Module.setSuspendCallback(calib_move.Suspend)
+    Module.setResumeCallback(calib_move.Resume)
     while True:
         calib_move.run()
         calib_move.print()
