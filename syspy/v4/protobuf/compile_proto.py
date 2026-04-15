71bb62262045e9d0fe60ee3db904b69345356675
xuyin <xuyin@seer-robotics.ai>
2026-04-15 10:59:44 +0800

    chore(rbk4): 更新 proto

--- syspy/v4/protobuf/compile_proto.py
+++ syspy/v4/protobuf/compile_proto.py
@@ -9,45 +9,44 @@ import subprocess
 import sys
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 compile_proto.py <input_dir>")
-        sys.exit(1)
+if len(sys.argv) != 2:
+    print("Usage: python3 compile_proto.py <input_dir>")
+    sys.exit(1)
 
-    input_dir = sys.argv[1]
-    output_dir = "./message"
+input_dir = sys.argv[1]
+output_dir = "./message"
 
-    for file in glob.glob(os.path.join(output_dir, "*_pb2.py")):
-        os.remove(file)
+for file in glob.glob(os.path.join(output_dir, "*_pb2.py")):
+    os.remove(file)
 
-    proto_files = glob.glob(os.path.join(input_dir, "*.proto"))
-    subprocess.call(
-        [
-            "protoc",
-            "--proto_path",
-            input_dir,
-            "--python_out",
-            output_dir,
-        ]
-        + proto_files
-    )
-    subprocess.call(
-        [
-            "protol",
-            "--create-package",
-            "--in-place",
-            "--python-out",
-            output_dir,
-            "protoc",
-            "--proto-path",
-            input_dir,
-        ]
-        + proto_files
-    )
-    subprocess.call(
-        [
-            "black",
-            "--quiet",
-            output_dir,
-        ]
-    )
+proto_files = glob.glob(os.path.join(input_dir, "*.proto"))
+subprocess.call(
+    [
+        "protoc",
+        "--proto_path",
+        input_dir,
+        "--python_out",
+        output_dir,
+    ]
+    + proto_files
+)
+subprocess.call(
+    [
+        "protol",
+        "--create-package",
+        "--in-place",
+        "--python-out",
+        output_dir,
+        "protoc",
+        "--proto-path",
+        input_dir,
+    ]
+    + proto_files
+)
+subprocess.call(
+    [
+        "black",
+        "--quiet",
+        output_dir,
+    ]
+)
