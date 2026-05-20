6a199107a4a782ae7736c37a88395123cba669bd
chenHongli <chenhl@seer-robotics.ai>
2026-05-20 16:14:44 +0800

    feat: m-6977954505 Trace.chart/log 适配动态 key

--- syspy/lib/trace.py
+++ syspy/lib/trace.py
@@ -1,4 +1,5 @@
 from abc import ABC
+from typing import Union
 
 from syspy.core.rbk_rpc import Service, RBKVersionError
 
@@ -6,7 +7,7 @@ from syspy.core.rbk_rpc import Service, RBKVersionError
 class TraceInterface(ABC, Service):
 
     @classmethod
-    def chart(cls, msg: dict, output_console: bool = False, output_time: bool = False, *, name: str = ""):
+    def chart(cls, msg: dict, output_console: bool = False, output_time: bool = False, *, name: str = "", debug: bool = False):
         """记录图表数据
 
         Args:
@@ -14,11 +15,12 @@ class TraceInterface(ABC, Service):
             output_console (bool): 是否开启控制台输出。默认不开启。
             output_time (bool): 是否在控制台打印时间。默认不开启。
             name (str): 图表名称。缺省为"chart", 否则 f"chart.{name}"
+            debug (bool): 是否为 debug 日志。True 表示 debug 模式下才落盘
         """
         raise RBKVersionError()
 
     @classmethod
-    def log(cls, msg: str, output_console: bool = True, output_time: bool = False, *, name: str = "", debug: bool = False):
+    def log(cls, msg: Union[str, dict], output_console: bool = True, output_time: bool = False, *, name: str = "", debug: bool = False):
         """记录日志
 
         Args:
