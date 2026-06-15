518190f0d0c69dec928cbc5f29e5054f2e947315
孙金淦 <sunjg@seer-robotics.ai>
2026-06-15 15:19:15 +0800

    fix: m-7015497393 删除设置安全激光相关接口，分离laser.py的接口层和应用层

--- syspy/laser.py
+++ syspy/laser.py
@@ -1,79 +1,7 @@
-from typing import List, Dict, Optional, Any, TYPE_CHECKING
-import json
-import time
-from syspy import Module, ScriptStatus, Trace, RobotParam, ScriptParam, RBK_VERSION
-from syspy.core.rbk_rpc import Service
-from syspy.utils.param_server import ParamType
-from syspy.utils import ScriptType
-if hasattr(ScriptParam, '_instance'):
-    ScriptParam._instance = None 
-    ScriptParam._initialized = False
-    ScriptParam.config_change_callback = None
-    ScriptParam.event_task_config = False
+# -*- coding: utf-8 -*-
+from typing import List, Optional, TYPE_CHECKING
 
-start_time = time.time()
-script_param = ScriptParam(__file__)
-
-class ConfigParams:
-    """脚本配置参数定义"""
-    device_name_key_map = None
-    simulation = None
-
-    @classmethod
-    def init(cls):
-        builder = script_param.builderConfig()
-        with builder.GROUPS():
-            with builder.GROUP(key="laser_config", name="激光配置", desc="激光控制相关配置"):
-                builder.TYPE(ParamType.ARRAY)
-
-                with builder.CHILDREN():
-                    with builder.CHILD(key="device_name_key_map", name="激光名称映射",
-                                       desc="激光名称到设备key的映射，JSON格式"):
-                        builder.TYPE(ParamType.STRING)
-                        builder.DEFAULTVALUE('{"laser_name_001": "laser__001", "laser_name_002": "laser_002"}')
-
-        # 保存配置参数，和原参数文件合并
-        builder.save(merge=True)
-        cls.load_config()
-
-    @classmethod
-    def load_config(cls):
-        """重新加载配置参数"""
-        config = script_param.loadConfig()
-        Trace.log(f"Loaded laser config: {config}")
-        cls.simulation = config.get("simulation", False)
-        try:
-            cls.device_name_key_map = json.loads(config.get("device_name_key_map", "{}"))
-            print(cls.device_name_key_map)
-        except json.JSONDecodeError:
-            cls.device_name_key_map = {"laser_name_001": "laser__001", "laser_name_002": "laser_002"}
-            Trace.log("解析激光名称映射配置失败，使用默认值")
-
-
-
-
-
-
-# 任务输入参数定义
-class InputParams:
-    """脚本任务输入参数定义"""
-    builder = script_param.builderInput()
-
-    with builder.GROUPS():
-        with builder.CHILD(key="key", name="激光Key", desc="激光（Laser）设备的 name。""表示选择全部激光。"):
-            builder.TYPE(ParamType.STRING)
-            builder.REQUIRED(False)
-        with builder.CHILD(key="name", name="激光名称", desc="激光设备的key。""表示选择全部激光。"):
-            builder.TYPE(ParamType.STRING)
-            builder.REQUIRED(False)
-        with builder.CHILD(key="enable", name="使能激光", desc="是否启用激光muting："):
-            builder.TYPE(ParamType.BOOL)
-            builder.REQUIRED(True)
-            builder.DEFAULTVALUE(True)
-
-
-
-    builder.save()
+from syspy import RBK_VERSION
 
 if TYPE_CHECKING:
     if RBK_VERSION == 3:
@@ -83,7 +11,7 @@ if TYPE_CHECKING:
 
 
 class LaserInterface:
-    """激光类"""
+    """激光接口类"""
 
     def __init__(self, topic=None):
         if RBK_VERSION == 3:
@@ -136,13 +64,11 @@ class LaserInterface:
 
     def clear2DLaserAngle(self, keys: List[str]):
         """清除激光设备角度
-        
+
         Args:
             keys (List[str]): 激光设备的key列表
         """
         self.child.clear2DLaserAngle(keys)
-
-    #----------------------------------------------------#
 
     def getNearestLaserPoint(self, key: str) -> List[float]:
         """获取与指定激光距离最近的激光点与激光中心的距离和朝向
@@ -171,13 +97,13 @@ class LaserInterface:
 
         Args:
             key (str): 激光设备的key。""表示选择全部激光。
-            enable (int): 表示是否启用激光muting，true启用，false禁用
+            enable (bool): 表示是否启用激光muting，true启用，false禁用
         """
         self.child.setSafeLaserMute(key, enable)
 
 
 class Laser3DInterface:
-    """激光类"""
+    """3D激光接口类"""
 
     def __init__(self, topic=None):
         if RBK_VERSION == 3:
@@ -206,120 +132,6 @@ class Laser3DInterface:
         """
         return self.child.getLasers3d()
 
+
 Laser: LaserInterface = LaserInterface()
 Laser3D: Laser3DInterface = Laser3DInterface()
-
-
-
-class ParamError(Exception):
-    """参数错误异常"""
-    pass
-
-
-
-class ParamCheck:
-    @staticmethod
-    def required(param_name: str, req: Dict[str, Any]) -> Any:
-        """检查必填参数"""
-        if param_name not in req:
-            raise ParamError(f"Missing required parameter: {param_name}")
-        return req[param_name]
-    
-    @staticmethod
-    def optional(param_name: str, req: Dict[str, Any],default: Optional[Any] = None) -> Optional[Any]:
-        return req.get(param_name, default)
-
-
-
-class LaserSafeController:
-    def __init__(self, device_name_key_map: Dict[str, str] = {}):
-        self.device_name_key_map: Dict[str, str] = device_name_key_map  
-    def _get_real_key(self, req: Dict[str, Any]) -> str:
-        if "key" in req:
-            return ParamCheck.required("key", req)
-        
-        if "name" in req:
-            name = ParamCheck.required("name", req)
-            if name not in self.device_name_key_map:
-                raise ParamError(f"Device name {name} not found")
-            return self.device_name_key_map[name]
-        
-        raise ParamError("need key or name")
-    def handle_robot_control_request(self, req: Dict[str, Any]) -> Dict[str, Any]:
-        try:
-            realKey= self._get_real_key(req)
-            cheackEnable= ParamCheck.required("enable", req)
-                # a=Laser.setSafeLaserMute(realKey, cheackEnable)
-            Service.client().call_service("DSPChassis", "setSafeLaserMute",realKey,cheackEnable)
-        except Exception as e:
-            Module.setStatus(ScriptStatus.FAILED)
-            Trace.log(f"setSafeLaserMute error: {str(e)}")
-            return {"code": -2, "msg": f"system error: {str(e)}"}
-
-        else:
-            Module.setStatus(ScriptStatus.FINISHED)
-            return {"code": 0, "msg": "success"}
-
-
-def script_config_callback():
-    """脚本配置参数修改回调"""
-    Trace.log("script_config_callback() - 激光配置参数更新")
-    ConfigParams.load_config()
-
-
-def robot_device_callback(change_devices: List[str]):
-    Trace.log(f"robot_device_callback({change_devices})")
-    for device in change_devices:
-        if device == "Laser":
-            Trace.log("激光设备参数更新，重新加载配置")
-            ConfigParams.load_config()
-
-
-
-def main():
-    ConfigParams.init()
-    ScriptParam.setConfigChangeCallBack(script_config_callback)
-    Module.init(script_type=ScriptType.TASK)
-
-    RobotParam.setDeviceChangeCallBack(robot_device_callback)
-    laser_controller = LaserSafeController()
-    count=0
-    while True:
-        status = Module.getStatus()          
-        if status in (ScriptStatus.RUNNING, ScriptStatus.NONE):
-            args = Module.getTaskArgs()
-
-            validated_params={}
-            # args={
-            #         "key": "Laser-001",
-            #         "enable": True,
-            #     }
-            if args:
-                try:
-                    Trace.log(f"接收到激光控制任务: {json.dumps(args, indent=2)}")
-                    validated_params = script_param.loadInput(args)
-                    print(validated_params)
-                except ValueError as e:
-                    Trace.log(f"参数验证失败: {e}")
-                    break
-                laser_controller.handle_robot_control_request(validated_params)
-            else:
-                Trace.log(f"激光参数为空")
-                count+=1
-                if count>10:
-                    Trace.log(f"激光参数为空，超过10次未收到任务,")
-                    break
-        elif status in (ScriptStatus.FAILED, ScriptStatus.FINISHED):
-            if status == ScriptStatus.FAILED:
-                Trace.log(f"激光控制任务 {Module.getStatus()} 失败")
-            else:
-                Trace.log(f"激光控制任务 {Module.getStatus()} 完成")
-            Module.setStatus(ScriptStatus.NONE)
-            break
-
-        # 防止CPU占用过高
-        time.sleep(0.1)
-
-
-if __name__ == '__main__':
-    main()
