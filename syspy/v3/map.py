3cd584044192991f76ff12200925a161af1ff939
mazj <mazj@seer-robotics.ai>
2026-08-05 17:28:47 +0800

    fix: m-7039239908 切换地图接口从MCLoc改为调用MF

--- syspy/v3/map.py
+++ syspy/v3/map.py
@@ -1,9 +1,20 @@
-from syspy.core.rbk_rpc import default_plugin, call_service
+from syspy.core.rbk_rpc import call_service, default_plugin
 from syspy.map import MapInterface
 
-@default_plugin("MCLoc")
+
+@default_plugin("MoveFactory")
 class MapV3(MapInterface):
     """地图类"""
+
+    _TOPIC = "rbk.protocol.msgMap"
+    _PLUGIN = "BlockMapLoader"
+    _MODEL_CLASS = None
+
+    @classmethod
+    def initModelClass(cls):
+        if cls._MODEL_CLASS is None:
+            from .protobuf.message.message_map_pb2 import msgMap
+            cls._MODEL_CLASS = msgMap
 
     @classmethod
     @call_service()
